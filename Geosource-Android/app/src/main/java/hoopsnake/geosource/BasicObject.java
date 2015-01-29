8f7468ecc97d8b26451140e44bd7e9ffb6ca8c88
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2015-01-29 12:40:11 -0600

    Added a basic mocking example

---
+++ Geosource-Android/app/src/main/java/hoopsnake/geosource/BasicObject.java
@@ -0,0 +1,13 @@
+package hoopsnake.geosource;
+
+/**
+ * Created by joshheinrichs on 15-01-28.
+ */
+public class BasicObject {
+
+    public int value()
+    {
+        return 1;
+    }
+
+}
--- Geosource-Android/app/src/test/java/hoopsnake/geosource/MainActivityTest.java
+++ Geosource-Android/app/src/test/java/hoopsnake/geosource/MainActivityTest.java
@@ -3,13 +3,15 @@ package hoopsnake.geosource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
+import org.mockito.*;
+
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
@@ -38,7 +40,13 @@ public class MainActivityTest {
         activity.coverage(2);
         activity.coverage(3);
         activity.coverage(4);
+    }
 
+    @Test
+    public void testValue() {
+        BasicObject object = Mockito.mock(BasicObject.class);
+        Mockito.when(object.value()).thenReturn(2);
+        assertEquals(object.value(), 2);
     }
 
 }
