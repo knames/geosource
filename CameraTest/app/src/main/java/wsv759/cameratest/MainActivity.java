07cc4aa407bcdd1ff6d571aeb02795b37dd6c96a
william-vdk <william.vanderkamp@gmail.com>
2015-02-02 03:21:44 -0600

    Added image-sending via asynctasks and sockets. Untested, due to lack of useable testing device.

--- CameraTest/app/src/main/java/wsv759/cameratest/MainActivity.java
+++ CameraTest/app/src/main/java/wsv759/cameratest/MainActivity.java
@@ -1,7 +1,10 @@
 package wsv759.cameratest;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -12,7 +15,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -101,8 +113,11 @@ public class MainActivity extends ActionBarActivity {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+                Uri fileUri = data.getData();
+                Toast.makeText(this, "Video saved to:\n" +
+                        fileUri, Toast.LENGTH_LONG).show();
+
+                new TaskSendImageOnSocket().execute(fileUri);
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
@@ -114,8 +129,11 @@ public class MainActivity extends ActionBarActivity {
         if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Video captured and saved to fileUri specified in the Intent
+                Uri fileUri = data.getData();
                 Toast.makeText(this, "Video saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+                        fileUri, Toast.LENGTH_LONG).show();
+
+                new TaskSendImageOnSocket().execute(fileUri);
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the video capture
             } else {
@@ -176,6 +194,86 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    enum SocketResult {SUCCESS, FAILED_CONNECTION, UNKNOWN_ERROR};
+
+    private class TaskSendImageOnSocket extends AsyncTask<Uri, Void, SocketResult>
+    {
+        protected SocketResult doInBackground(Uri... params) {
+            //this method will be running on background thread so don't update UI frome here
+            //do your long running http tasks here,you dont want to pass argument and u can access the parent class' variable url over here
+            String ipaddress = "localhost";
+            int portNum = 0;
+
+            OutputStream out; //wrapped stream to client
+            ObjectInputStream in; //stream from client
+            Socket outSocket;
+
+            try //create socket
+            {
+                outSocket = new Socket(InetAddress.getByName(ipaddress), portNum);
+                if (outSocket.isConnected())
+                    Log.i(getString(R.string.app_name),"Connection Established");
+                out = new ObjectOutputStream(outSocket.getOutputStream());
+                in = new ObjectInputStream(outSocket.getInputStream());
+                Log.i(getString(R.string.app_name), "Stream Created");
+            }
+            catch(IOException e)
+            {
+                Log.e(getString(R.string.app_name),"Shit got no connection, son!");
+                e.printStackTrace();
+
+                return SocketResult.FAILED_CONNECTION; //end program if connection failed
+            }
+
+            File imageFile = new File(params[0].getPath());
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(imageFile);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            Bitmap bm = BitmapFactory.decodeStream(fis);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] imageByteArray = stream.toByteArray();
+
+            try
+            {
+                Log.d(getString(R.string.app_name), "Attempting to send image " + params[0].getPath());
+                out.write(imageByteArray);
+
+                Log.i(getString(R.string.app_name), "Connection Closing");
+                in.close();
+                out.close();
+                outSocket.close();
+                Log.i(getString(R.string.app_name), "Connection Closed");
+            }
+            catch (IOException e)
+            {
+                Log.e(getString(R.string.app_name),"Unknown Error");
+                e.printStackTrace();
+
+                return SocketResult.UNKNOWN_ERROR;
+            }
+
+            return SocketResult.SUCCESS;
+        }
+
+        protected void onPostExecute(SocketResult result) {
+            switch (result) {
+                case UNKNOWN_ERROR:
+                    Toast.makeText(MainActivity.this, "File upload failed. Unknown error.", Toast.LENGTH_SHORT).show();
+                    break;
+                case FAILED_CONNECTION:
+                    Toast.makeText(MainActivity.this, "File upload failed. Connection failed.", Toast.LENGTH_SHORT).show();
+                    break;
+                case SUCCESS:
+                    Toast.makeText(MainActivity.this, "New file uploaded.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     /* Checks if external storage is available for read and write. */
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
