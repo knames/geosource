d43cd90559e3f2acc40d59dc748b7b3326529cbf
linxcat <clavoy_comp@hotmail.com>
2015-02-17 15:43:27 -0600

    Some work on the server side packet implementation, added the two other required fields. Should be able to read it on the android side, populate the hashmap, and send it right back

--- HoopSnake-Geosource/src/Communication/Packet.java
+++ HoopSnake-Geosource/src/Communication/Packet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
- *
+ * A container for sending incident posts between the android app and the server.
+ * Meant to be serialized and sent using an ObjectOutputStream.
  * @author Connor
  */
 public class Packet implements Serializable{
@@ -15,15 +17,31 @@ public class Packet implements Serializable{
     //change this if and only if a new implementation is incompatible with an old one
     private static final long serialVersionUID = 1L;
     
-    private void readObject(ObjectInputStream in) throws ClassNotFoundException, IOException
+    public static enum listFields{Picture, String, Video, Sound};
+    
+    private LinkedList<String> fieldNames;
+    private LinkedList<listFields> fieldTypes;
+    private HashMap<String, Object> packetContents;
+    
+    public Packet(LinkedList<String> fieldNames, LinkedList<listFields> fieldTypes)
     {
-        
+        this.fieldNames = fieldNames;
+        this.fieldTypes = fieldTypes;
     }
     
-     private void writeObject(ObjectOutputStream out) throws IOException
-     {
-        
-     }
+    private void readObject(ObjectInputStream in) throws ClassNotFoundException, IOException
+    {
+        fieldNames = (LinkedList<String>)in.readObject();
+        fieldTypes = (LinkedList<listFields>)in.readObject();
+        packetContents = (HashMap<String, Object>)in.readObject();
+    }
+    
+    private void writeObject(ObjectOutputStream out) throws IOException
+    {
+        out.writeObject(fieldNames);
+        out.writeObject(fieldTypes);
+        out.writeObject(packetContents);
+    }
     
     
 }
