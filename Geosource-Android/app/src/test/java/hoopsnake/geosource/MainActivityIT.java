0827cd2f5d8fefada9f852d15f425fb18a9017d0
RetroHack <wwf594@mail.usask.ca>
2015-02-21 19:32:12 -0600

    Added Basic System tests. Started on system tests by adding one for the non-functional audio button. Should be able to finish up a system test for the current system so far by the end of the day.

--- Geosource-Android/app/src/test/java/hoopsnake/geosource/MainActivityIT.java
+++ Geosource-Android/app/src/test/java/hoopsnake/geosource/MainActivityIT.java
@@ -15,6 +15,52 @@ public class MainActivityIT extends ActivityInstrumentationTestCase2<MainActivit
         super(MainActivity.class);
     }
 
+    //Just a basic test I wrote to look at things.
+    /*public void testIfTextAppears() throws Exception
+    {
+        //Check to see if we are in the right activity
+        solo.assertCurrentActivity("wrong activity", MainActivity.class);
+
+        solo.clickOnButton(solo.getString(R.string.add_audio));
+
+        //Cool fact: It will accept partial matches: i.e if Hello World is on the screen, looking for Hello asserts to true.
+        assertTrue(solo.searchText("Hello"));
+    }*/
+
+    /* Currently tests these things:
+    *  Checks to see if Audio Button can be pressed and outputs proper error message.
+    *  Checks to see if Camera can successfully save photo
+    *  Checks to see if Camera can successfully save video
+     */
+    //Checks to see if basic functionality is working.
+    public void testSanity() throws Exception
+    {
+        //Check to see if we are in the right activity
+        solo.assertCurrentActivity("wrong activity", MainActivity.class);
+        this.testAudio();
+        assertTrue(1==2);
+        //cameraTest();
+        //VideoTest();
+
+    }
+
+    //For now, tests to see if the button can be clicked and gives the proper error message.
+    //Audio recordings have not been implemented yet.
+    public void testAudio() throws Exception
+    {
+        //Check to see if we are in the right activity
+        solo.assertCurrentActivity("wrong activity", MainActivity.class);
+
+        solo.clickOnButton(solo.getString(R.string.add_audio));
+
+        //Cool fact: It will accept partial matches: i.e if Hello World is on the screen, looking for Hello asserts to true.
+        assertTrue(solo.searchText("audio recording not yet implemented"));
+    }
+
+
+
+
+
     @Override
     public void setUp() throws Exception {
         solo = new Solo(getInstrumentation(), getActivity());
