70f6055bd0297468b5d2f1bc42ab6f0832291036
linxcat <clavoy_comp@hotmail.com>
2015-03-01 16:39:08 -0600

    Database connection specs ready for fill-in. Will continue work in the mean time

--- HoopSnake-Geosource/src/DataBase/DBAccess.java
+++ HoopSnake-Geosource/src/DataBase/DBAccess.java
@@ -1,10 +1,46 @@
 package DataBase;
 
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Connor
  */
 public class DBAccess {
+    
+    private Connection dbconnection;
+    
+    public DBAccess()
+    {
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            System.out.println("fail");
+        }
+        
+        try
+        {
+            dbconnection = DriverManager.getConnection
+            ("jdbc:postgresql://SERVER_URL:PORTNUM/", "DB_NAME",
+            "PASSWORD");
+        }
+        catch(SQLException sqle)
+        {
+            System.out.println("Error connecting to Database...");
+            System.out.println(sqle.getMessage());
+        }
+    }
+
     public String getFormSpecLocation(String channelName)
     {
         return null;
