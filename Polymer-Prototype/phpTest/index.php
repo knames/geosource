8bc12efd657f9610222de22cf780aad719c1227b
knames <okenso@gmail.com>
2015-03-04 00:04:17 -0600

    changing database to use a hashed identifier
    change and move some files. Database should now be able to store a
    hashed identifier.

---
+++ Polymer-Prototype/phpTest/index.php
@@ -0,0 +1,36 @@
+<? php 
+
+
+//db credentials
+$servername = "okenso.com";
+$username = "hdev";
+$dbname = "hdev";
+$password = "devsnake371";
+
+
+
+?>
+
+<html>
+<head>
+	
+</head>
+<body>yolo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
