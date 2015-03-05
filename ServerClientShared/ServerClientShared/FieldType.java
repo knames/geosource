856bed276e6f1f8a6519753df97523e42986f4c3
linxcat <clavoy_comp@hotmail.com>
2015-03-05 15:33:37 -0600

    Rolling back changes to accommodate new design ideas

--- ServerClientShared/ServerClientShared/FieldType.java
+++ ServerClientShared/ServerClientShared/FieldType.java
@@ -1,55 +1,15 @@
 package ServerClientShared;
 
-import java.util.ArrayList;
-
 /**
  * Created by wsv759 on 18/02/15.
  * This enumerates the types of objects (or files) that could constitute
  * the content for a field.
  */
 public enum FieldType {
-    IMAGE{
-
-        @Override
-        public void populateUI(UITYPE UI) {
-            UI.addImage();
-        }
-        
-    },
-    STRING{
-
-        @Override
-        public void populateUI(UITYPE UI) {
-            UI.addString();
-        }
-        
-    },
-    VIDEO{
-
-        @Override
-        public void populateUI(UITYPE UI) {
-            UI.addVideo();
-        }
-        
-    },
-    AUDIO{
-
-        @Override
-        public void populateUI(UITYPE UI) {
-            UI.addAudio();
-        }
-        
-    },
-    OPTION_LIST{
-        
-        public ArrayList<String> options;        
-        
-        @Override
-        public void populateUI(UITYPE UI) {
-            UI.addOptions(options);
-        }
-    }
-    ; // <-- end of type/implementation definitions
-    
-    public abstract void populateUI(UITYPE UI);
+    IMAGE,
+    STRING,
+    VIDEO,
+    AUDIO,
+    OPTION_LIST
+    ;
 }
