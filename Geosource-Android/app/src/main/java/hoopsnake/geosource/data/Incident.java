dc546836fe8649eb7d6b13fcd47feec77c9892fa
william-vdk <william.vanderkamp@gmail.com>
2015-03-07 16:42:48 -0600

    added the incident to the serverclientshared folder. Couldn't decide on constructor so didn't include one.

--- Geosource-Android/app/src/main/java/hoopsnake/geosource/data/Incident.java
+++ Geosource-Android/app/src/main/java/hoopsnake/geosource/data/Incident.java
@@ -1,48 +1,17 @@
 package hoopsnake.geosource.data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
-
-import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by wsv759 on 19/02/15.
  */
-public class Incident {
+public class Incident implements Serializable {
 
     /** The fields for this incident, including their content. Their content may be null. */
-    ArrayList<FieldWithContent> fieldList;
-
-    /** Create a new incident by populating its fieldList by means of a
-     * fieldWithoutContentList, and adding null content. */
-    public Incident(ArrayList<FieldWithoutContent> fieldWithoutContentList)
-    {
-        fieldList = new ArrayList<FieldWithContent>();
-
-        for (FieldWithoutContent fieldWithoutContent : fieldWithoutContentList)
-        {
-            FieldWithContent newFieldWithContent = new FieldWithContent(
-                    fieldWithoutContent.getTitle(), fieldWithoutContent.getType(), fieldWithoutContent.isRequired());
-            fieldList.add(newFieldWithContent);
-        }
-    }
+    protected ArrayList<FieldWithContent> fieldList;
 
     public ArrayList<FieldWithContent> getFieldList() {
         return fieldList;
-    }
-
-    /** Is the incident ready to be shipped off? That is, has it been completely filled out? */
-    public boolean isCompletelyFilledIn()
-    {
-        assertNotNull(fieldList);
-
-        for (FieldWithContent fieldWithContent : fieldList)
-        {
-            assertNotNull(fieldWithContent);
-
-            if (!fieldWithContent.contentIsFilled() && fieldWithContent.isRequired())
-                return false;
-        }
-
-        return true;
     }
 }
