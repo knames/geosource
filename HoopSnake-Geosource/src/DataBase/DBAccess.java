a531dad48418665451c634a9d5e7b0b064ed06d5
linxcat <clavoy_comp@hotmail.com>
2015-03-08 16:25:57 -0600

    Basic post functionality completed, testing to follow

--- HoopSnake-Geosource/src/DataBase/DBAccess.java
+++ HoopSnake-Geosource/src/DataBase/DBAccess.java
@@ -1,5 +1,6 @@
 package DataBase;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -48,17 +49,48 @@ public class DBAccess {
     public String getFormSpecLocation(String channelName)
     {
         String filePath = null;
-        try
-        {
-            Statement statement = dbconnection.createStatement();
+        try (Statement statement = dbconnection.createStatement()) {
             ResultSet results = statement.executeQuery(Queries.getFormSpec(channelName));
             filePath = results.getNString(1);
-            statement.close();
         }
         catch (SQLException SQLe)
         {
             System.out.println("formSpec not found!! Possible broken Incident request");
         }
         return filePath;
+    }
+    
+    /**
+     * saves a string to it's field position on a channel's database table
+     * @param channelName the channel who's table will be saved to
+     * @param fieldName the name of the field we are saving
+     * @param content the string to save
+     */
+    public void saveStringField(String channelName, String fieldName, String content)
+    {
+        try (Statement statement = dbconnection.createStatement()) {
+            statement.execute(Queries.saveStringField(channelName, fieldName, content));
+        }
+        catch (SQLException SQLe)
+        {
+            System.out.println("Saving string field failed");
+        }
+    }
+    
+    /**
+     * saves the filepath to the database for a picture previously saved to the filesystem
+     * @param channelName the name of the channel being posted to
+     * @param fieldName the title of this picture's field
+     * @param filePath the filepath to be posted
+     */
+    public void savePictureField(String channelName, String fieldName, String filePath)
+    {
+        try (Statement statement = dbconnection.createStatement()) {
+            statement.execute(Queries.savePictureField(channelName, fieldName, filePath));
+        }
+        catch (SQLException SQLe)
+        {
+            System.out.println("Saving picture field failed");
+        }
     }
 }
