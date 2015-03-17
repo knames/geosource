c39eaf905b0634525a8392145e8688d79e65d5e2
linxcat <clavoy_comp@hotmail.com>
2015-03-17 17:37:34 -0600

    Fixed Tabs... KEN and took out anti-pwd code

--- HoopSnake-Geosource/src/FileSystem/FileAccess.java
+++ HoopSnake-Geosource/src/FileSystem/FileAccess.java
@@ -24,68 +24,65 @@ import java.util.UUID;
  */
 public class FileAccess {
 
-	public FileAccess() throws URISyntaxException {
+    public FileAccess() throws URISyntaxException {
 
-		try {
-			FolderExists();
-		} catch (Exception e) {
-			System.out.println("Failed to check/create folders!");
-			e.printStackTrace();
-		}
-		// System.out.println("Home Directory is " + homeDirectory);
-		// System.out.println("would find specs at " + homeDirectory +
-		// "media/spec/okenso.6");
-	}
+        try {
+            FolderExists();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Failed to check/create folders!");
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Checks if the media/spec folder exists..
-	 * 
-	 * @return true if there, false otherwise
-	 * @throws Exception
-	 */
-	private void FolderExists() {
-		String[] folderNames = { "media", "media/spec", "media/audio",
-				"media/video", "media/image", "media/string" };
+    /**
+     * Checks if the media/spec folder exists..
+     * 
+     * @return true if there, false otherwise
+     * @throws Exception
+     */
+    private void FolderExists() {
+        String[] folderNames = { "media", "media/spec", "media/audio",
+                        "media/video", "media/image", "media/string" };
 
-		for (int i = 0; i < folderNames.length; i++) {
-			Path folder = Paths.get(folderNames[i]);
-			if (Files.notExists(folder, LinkOption.NOFOLLOW_LINKS)) {
-				File dir = new File(folderNames[i]);
-				dir.mkdir();
-				System.out.println(folderNames[i]
-						+ " has been created.");
-			} else {
-				System.out.println(folderNames[i] + " exists!");
-			}
-		}
-	}
+        for (int i = 0; i < folderNames.length; i++) {
+            Path folder = Paths.get(folderNames[i]);
+            if (Files.notExists(folder, LinkOption.NOFOLLOW_LINKS)) {
+                File dir = new File(folderNames[i]);
+                dir.mkdir();
+                System.out.println(folderNames[i]
+                                    + " has been created.");
+            } else {
+                System.out.println(folderNames[i] + " exists!");
+            }
+        }
+    }
 
-	public ArrayList<FieldWithoutContent> getFormSpec(String fileName) {
-		FileInputStream fileRead;
-		ObjectInputStream in;
-		String filePath = homeDirectory + fileName;
-		try {
-			fileRead = new FileInputStream(filePath); // file input
-			in = new ObjectInputStream(fileRead); // stream to object
-			return (ArrayList<FieldWithoutContent>) in.readObject();
-		} catch (IOException IOe) {
-			System.err.println("Reader initialization failed!");
-			return null;
-		} catch (ClassNotFoundException CNFe) {
-			System.err.println("Parsing Specification failed");
-			return null;
-		}
-	}
+    public ArrayList<FieldWithoutContent> getFormSpec(String fileName) {
+        FileInputStream fileRead;
+        ObjectInputStream in;
+        try {
+            fileRead = new FileInputStream("media/spec/" + fileName); // file input
+            in = new ObjectInputStream(fileRead); // stream to object
+            return (ArrayList<FieldWithoutContent>) in.readObject();
+        } catch (IOException IOe) {
+            System.err.println("Reader initialization failed!");
+            return null;
+        } catch (ClassNotFoundException CNFe) {
+            System.err.println("Parsing Specification failed");
+            return null;
+        }
+    }
 
-	public String saveField(Serializable fieldContent) {
-		UUID fileUUID = UUID.randomUUID();
-		String fileName = fileUUID.toString();
-		try (ObjectOutputStream fileOut = new ObjectOutputStream(
-				new FileOutputStream(fileName))) {
-			fileOut.writeObject(fieldContent);
-		} catch (IOException IOe) {
-			System.err.println("Error attempting to write field");
-		}
-		return fileName;
-	}
+    public String saveField(Serializable fieldContent) {
+        UUID fileUUID = UUID.randomUUID();
+        String fileName = fileUUID.toString();
+        try (ObjectOutputStream fileOut = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            fileOut.writeObject(fieldContent);
+        } catch (IOException IOe) {
+            System.err.println("Error attempting to write field");
+        }
+        return fileName;
+    }
 }
