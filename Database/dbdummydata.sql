873455c27e8971d6a627ad9bc576bdfe55ad39e6
knames <okenso@gmail.com>
2015-03-17 18:36:11 -0600

    null the time field

--- Database/dbdummydata.sql
+++ Database/dbdummydata.sql
@@ -18,7 +18,7 @@ insert into channelfavs values ("okenso", "okenso's channel", "okenso");
 CREATE TABLE posts_okenso_pothole (
 	p_poster varchar(25) NOT NULL,
 	p_number INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	p_time DATETIME NOT NULL,
+	p_time DATETIME,
 	p_field1 varchar(50), # will be named after the title
 	p_field2 varchar(50), # how big should these be?
 	FOREIGN KEY (p_poster) REFERENCES users (u_username)
