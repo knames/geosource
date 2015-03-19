85e15aa2d9986613ff573cd06dfeba4dcb221e6a
knames <okenso@gmail.com>
2015-03-18 23:55:00 -0600

    phpoly galore
    nearly finished php user class
    
    modified the DB a bunch

--- Database/dbinit.sql
+++ Database/dbinit.sql
@@ -15,10 +15,13 @@ CREATE TABLE channels (
 	);
 
 CREATE TABLE channelmods (
-	cm_m_username varchar(25) NOT NULL,
-	cm_m_name varchar(50) NOT NULL,
-	FOREIGN KEY (cm_m_username) REFERENCES users (u_username),
-	FOREIGN KEY (cm_m_name) REFERENCES channels (ch_name)
+	cm_username varchar(25) NOT NULL,
+	cm_chname varchar(50) NOT NULL,
+	cm_chowner varchar(25) NOT NULL,
+	FOREIGN KEY (cm_username) REFERENCES users (u_username),
+	FOREIGN KEY (cm_chname) REFERENCES channels (ch_name),
+	FOREIGN KEY (cm_chowner) REFERENCES users (u_username),
+	PRIMARY KEY (cm_username, cm_chname, ch_chowner)
 	);
 
 CREATE TABLE channelfavs (
@@ -27,25 +30,39 @@ CREATE TABLE channelfavs (
 	ch_fav_chowner varchar(25) NOT NULL,
 	FOREIGN KEY (ch_fav_username) REFERENCES users (u_username),
 	FOREIGN KEY (ch_fav_chname) REFERENCES channels (ch_name),
-	FOREIGN KEY (ch_fav_chowner) REFERENCES users (u_username)
+	FOREIGN KEY (ch_fav_chowner) REFERENCES users (u_username),
+	PRIMARY KEY (ch_fav_username, ch_fav_chname, ch_fav_chowner)
 	);
 
-CREATE TABLE posts_okenso_march13 (
- p_poster varchar(25) NOT NULL,
- p_number INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
- p_TitleText varchar(100),
- p_time DATETIME,
- p_PictureField varchar(100),
- FOREIGN KEY (p_poster) REFERENCES users (u_username)
- );
 
 CREATE TABLE admin (
 	a_username varchar(25) PRIMARY KEY,
 	FOREIGN KEY (a_username) REFERENCES users (u_username)
 	);
 
+CREATE TABLE private_view_channels (
+	prv_username varchar(25) NOT NULL,
+	prv_chname varchar(50) NOT NULL,
+	prv_chowner varchar(25) NOT NULL,
+	FOREIGN KEY (prv_username) REFERENCES users (u_username),
+	FOREIGN KEY (prv_chname) REFERENCES channels (ch_name),
+	FOREIGN KEY (prv_chowner) REFERENCES users (u_username),
+	PRIMARY KEY (prv_username, prv_chname, prv_chowner)
+	);
+
+CREATE TABLE users_fav_posts (
+	ufp_username varchar(25) NOT NULL,
+	ufp_chname varchar(50) NOT NULL,
+	ufp_chowner varchar(25) NOT NULL,
+	ufp_number int UNSIGNED NOT NULL,
+	FOREIGN KEY (ufp_username) REFERENCES users (u_username),
+	FOREIGN KEY (ufp_chname) REFERENCES channels (ch_name),
+	FOREIGN KEY (ufp_chowner) REFERENCES users (u_username),
+	PRIMARY KEY (ufp_username, ufp_chname, ufp_chowner, ufp_number)
+	);
 
 
+-- comment this out when done with dummydata
 source /var/www/okenso.com/cmpt371group2/Database/dbdummydata.sql
 
 /** Sample Table
--- Polymer-Prototype/components/phpoly/dbgetusername.php
+++ Polymer-Prototype/components/phpoly/dbgetusername.php
@@ -9,7 +9,6 @@
 
 	$userid = '0001'; // make sure it's the hashed value
 
-
 	// Create connection
 	$mysqli = new mysqli($servername, $username, $password, $dbname);
 
@@ -19,13 +18,33 @@
 	} 
 	//echo "Connected successfully <br>";
 
+
+
+	/** get username */
  	$result = $mysqli->query("SELECT * FROM users WHERE u_identity = \"$userid\";");
 	$uname = $result->fetch_assoc();
  	//echo $row['u_username'];
  	$uname = $uname['u_username'];
 
+ 	/** returns an array of the required info
+ 	@channel is the name of the channel
+ 	@schname is the select of the row name 
+ 	@schowner is the select of the row owner
+ 	@wuser is the username we want to match */
+	function querytoarray ($channel,  $schname, $schowner, $wuser){
+		global $mysqli, $uname;
+		$result = $mysqli->query("SELECT $schname, $schowner FROM $channel 
+				WHERE $wuser = \"$uname\";");
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)){
+			$rows[] = array('name'=>$r[$schname],'owner' => $r[$schowner]);
+		}
+		return $rows;
+	}	
+
 
  	if (!$uname == null){
+ 		/** check if admin */
  		$result = $mysqli->query("SELECT a_username FROM admin WHERE a_username = \"$uname\";");
 		$aname = $result->fetch_assoc();
 		$aname = $aname['a_username'];
@@ -34,8 +53,36 @@
 		} else {
 			$aname = FALSE;
 		}
+
+		/** find subscribed channels */
+		$sub = querytoarray("channelfavs", "ch_fav_chname", "ch_fav_chowner", "ch_fav_username");
+
+		/** check private channel subs */
+		$prv = querytoarray("private_view_channels", "prv_chname", "prv_chowner", "prv_username");
+
+		/** check moderator status */
+		$mod =  querytoarray("channelmods", "cm_chname", "cm_chowner", "cm_username");
+
+		/** check owner status */
+		//select table_name from information_schema.tables where table_name LIKE '%_okenso_%';
+ 		$result = $mysqli->query("SELECT table_name FROM information_schema.tables WHERE table_name LIKE \"%$uname%\";");
+ 		$rows = array();
+ 		while ($r = mysqli_fetch_assoc($result)){
+ 			$table = $r['table_name'];
+ 			//print $table;
+ 			/** The monstrosity below strips posts_ and _username from the table names.*/
+ 			$rows[] = array('name'=>str_replace($uname."_", '',str_replace("posts_",'',$table)), 'owner'=>$uname);
+ 		}
+ 		$own = $rows;
+		//SAMPLE str_replace("world","Peter","Hello world!"); 
+
+		/** TODO: do favorites array of channel, chowner, p#, similar to the query method but 3 in the array */
+
+
+
 		//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-		$arr = array('username' => $uname, 'admin' => $aname);
+		$arr = array('username' => $uname, 'admin' => $aname, 'subscriber' => $sub, 
+			'viewer' => $prv, 'moderator'=>$mod, 'owner'=>$own);
 
 		
 		print "[".json_encode($arr)."]";
@@ -48,6 +95,8 @@
 
 
 	//print json_encode("\"username\":".$row['u_username']);
+
+
 
 
 	$mysqli->close();
