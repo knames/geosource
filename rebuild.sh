b7d42f230955f225d103fae86f5dc22bc4e69512
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2015-03-30 02:30:16 -0600

    Moved images, audio, videos into media folder

--- rebuild.sh
+++ rebuild.sh
@@ -10,6 +10,7 @@ DROP TABLE users_fav_posts CASCADE;
 DROP TABLE post_comments CASCADE;
 DROP TABLE channelfavs CASCADE;
 DROP TABLE channelmods CASCADE;
+DROP TABLE channel_banned CASCADE;
 DROP TABLE channels CASCADE;
 DROP TABLE users CASCADE;
 EOF
