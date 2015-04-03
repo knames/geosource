fbc3b6b834796cb5ef225c59bdad851bf5502f32
Ken <okenso@gmail.com>
2015-04-03 03:29:10 -0400

    Channel creation now works as long as fields are not added. Fieldnames will have to be synchronized between the server and the website before channels with fields can be created.

--- Polymer-Prototype/php/channel/create-channel.php
+++ Polymer-Prototype/php/channel/create-channel.php
@@ -1,11 +1,43 @@
 <?php 
 	//db credentials
+	$servername = "okenso.com";
+	$usernamme = "hdev";
+	$dbname = "dev";
+	$password = "devsnake371";
 
-	$socket = socket_create(AF_INET, SOCK_STREAM, 0) //Creating a TCP socket
-    	or die("error: could not create socket\n");
+	$data = json_decode(file_get_contents('php://input'), true);
 
-	socket_connect($socket, 'localhost', 9001) //Connecting to to server using that socket
-		or die("error: could not connect to host\n");
+	$channelname = $data["name"];
+	$channelowner = $data["owner"];
+	
+	$err = false;
+	$errMsg = "";
 
-	echo "Connected";
+	if (empty($channelname)) {
+		$err = true;
+		$errMsg  = "Please enter a channel name";
+	} else if (!preg_match("/^[a-zA-Z0-9]*$/", $channelname)) {
+		$err = true;
+		$errMsg = "Only letters and numbers allowed.";
+//	} else if (channelExists($channelname, $channelowner)) {
+//		$err = true;
+//		$errMsg = "You've already created a channel with that name.";
+	} else {
+		$socket = socket_create(AF_INET, SOCK_STREAM, 0) //Creating a TCP socket
+    			or die("error: could not create socket\n");
+
+		socket_connect($socket, 'localhost', 9001) //Connecting to to server using that socket
+			or die("error: could not connect to host\n");
+	
+		socket_write($socket, chr(2), 1);
+	
+		$command = '[{"command": "CREATE_CHANNEL"},';
+		socket_write($socket, $command, strlen($command));
+
+		$channel = json_encode($data)."]";
+		socket_write($socket, $channel, strlen($channel));
+	}
+
+	$resp = array('error' => $err, 'message' => $errMsg, 'channel' => $data);
+	echo json_encode($resp);
 ?>
