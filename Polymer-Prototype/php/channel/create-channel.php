bf6d4db368fae13733ed81057cee469ec5bbcc45
Ken <okenso@gmail.com>
2015-04-05 21:10:53 -0400

    (Josh) Can't figure out how to grab field labels in php, reverting until I have time Was attempting to verify uniqueness of field labels in PHP

--- Polymer-Prototype/php/channel/create-channel.php
+++ Polymer-Prototype/php/channel/create-channel.php
@@ -10,10 +10,19 @@
 	$channelname = $data["name"];
 	$channelowner = $data["owner"];
 	
-	$error = false;
-	$error_message = "";
+	$err = false;
+	$errMsg = "";
 
-	if(legalChannel($channelname) && legalFields($fields)) {
+	if (empty($channelname)) {
+		$err = true;
+		$errMsg  = "Please enter a channel name";
+	} else if (!preg_match("/^[a-zA-Z0-9]*$/", $channelname)) {
+		$err = true;
+		$errMsg = "Only letters and numbers allowed.";
+//	} else if (channelExists($channelname, $channelowner)) {
+//		$err = true;
+//		$errMsg = "You've already created a channel with that name.";
+	} else {
 		$socket = socket_create(AF_INET, SOCK_STREAM, 0) //Creating a TCP socket
     			or die("error: could not create socket\n");
 
@@ -29,49 +38,6 @@
 		socket_write($socket, $channel, strlen($channel));
 	}
 
-	$resp = array('error' => $error, 'error_message' => $error_message, 'channel' => $data);
+	$resp = array('error' => $err, 'message' => $errMsg, 'channel' => $data);
 	echo json_encode($resp);
-
-	function legalString($string) {
-		globals $error, $error_message;
-		if (empty($channelname)) {
-			$error = true;
-			$error_message  = "Cannot be empty.";
-			return false;
-		} else if (!preg_match("/^[a-zA-Z0-9]*$/", $channelname)) {
-			$error = true;
-			$error_message = "Only letters and numbers allowed.";
-			return false;
-		}
-		return true;
-	}
-
-	function legalChannel($channelname) {
-		globals $error, $error_message;
-		if(!legalString($channelname)) {
-			$error_message = "Illegal channel name. " . $error_message;
-			return false;
-		}
-		return true;
-	}
-
-	function legalFields($fields) {
-		globals $error, $error_message;
-		foreach ($fields as $field) {
-			if(!legalString($field)) {
-				$error_message = "Illegal field name. " . $error_message;
-				return false;
-			} else if ($field == "poster" || $field == "number" || $field == "title" || $field == "geotag") {
-				$error = true;
-				$error_message = "Illegal field name."
-				return false;
-			}		
-		}
-		if(count($fields) < count(array_unique($fields))) {
-			$error = true;
-			$error_message = "Illegal field name. All field names must be unique.";
-			return false;
-		}
-		return true;
-	}
 ?>
